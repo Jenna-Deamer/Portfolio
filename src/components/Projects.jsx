a85e66c02fc5939d8a6f554c85c6ca16609c8627
Jenna Deamer <jennadeamer@gmail.com>
2024-12-18 21:25:13 -0500

    Refactor Projects component to use dynamic project cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
 import "../styles/Projects.css";
-
+import projectCards from "../data/ProjectData";
 function Projects() {
   return (
     <>
@@ -7,53 +7,27 @@ function Projects() {
         <h2 className="text-center">My Work</h2>
 
         <section className="projects-container">
-          <article className="projects-card">
-            <div className="project-thumbnail">
-              <img
-                src="../../public/thumbnails/SmartBeachThumbnail.webp"
-                alt="Smart Beach Thumbnail"
-              />
-              <div className="project-title">
-                <h3>Smart Beach</h3>
+          {projectCards.map((card, index) => (
+            <article key={index} className="projects-card">
+              <div className="project-thumbnail">
+                <img
+                  src= {card.imgSrc}
+                  alt= {card.imgAlt}
+                  aria-label= {card.AriaLabel}
+                />
+                <div className="project-title">
+                  <h3>{card.title}</h3>
+                </div>
               </div>
-            </div>
-            <div className="text-content">
-              Smart Beach was a group project dedicated to enhancing safety at
-              Station Beach in Kincardine, Ontario. Leveraging machine learning
-              models and APIs developed by students specializing in Machine
-              Learning and Big Data at Georgian College, our team crafted a
-              predictive-based solution accessible through a user-friendly app
-              interface. By integrating real-time data on water and weather
-              conditions, our solution empowers beach visitors with critical
-              insights for a safer, more enjoyable experience.
-            </div>
-            <div className="button-container">
-              <a href="#" className="cta-button">Demo</a>
-              <a href="#" className="cta-button">Github</a>
-            </div>
-          </article>
-          <article className="projects-card">
-            <div className="project-thumbnail">
-              <img
-                src="../../public/thumbnails/SmartBeachThumbnail.webp"
-                alt="Smart Beach Thumbnail"
-              />
-              <div className="project-title">
-                <h3>Smart Beach</h3>
+              <div className="project-text-content">
+               {card.description}
               </div>
-            </div>
-            <div className="text-content">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nisi
-              iusto dolore sequi eius aut eligendi similique officia
-              perspiciatis? Modi doloremque eius odio quidem pariatur porro
-              nihil molestias incidunt delectus!
-            </div>
-            <div className="button-container">
-              <a href="#" className="cta-button">Demo</a>
-              <a href="#" className="cta-button">Github</a>
-            </div>
-          </article>
-  
+              <div className="button-container">
+                <a href={card.demoUrl} className="cta-button">Demo</a>
+                <a href={card.githubUrl} className="cta-button">Github</a>
+              </div>
+            </article>
+          ))}
         </section>
       </section>
     </>
---
+++ src/data/ProjectData.tsx
@@ -0,0 +1,24 @@
+const projectCards = [
+  {
+    title: "Smart Beach",
+    imgSrc: "/thumbnails/SmartBeachThumbnail.webp",
+    imgAlt: "Smart Beach Thumbnail",
+    description:
+      "Smart Beach was a group project dedicated to enhancing safety at Station Beach in Kincardine, Ontario. Leveraging machine learning models and APIs developed by students specializing in Machine Learning and Big Data at Georgian College, our team crafted a predictive-based solution accessible through a user-friendly app interface. By integrating real-time data on water and weather conditions, our solution empowers beach visitors with critical insights for a safer, more enjoyable experience. ",
+    githubUrl: "https://github.com/C-Blouin/SmartBeach",
+    demoUrl: "https://smartbeach.vercel.app/",
+    AriaLabel: "Visit live Demo of Smart Beach",
+  },
+  {
+    title: "Budget Capital",
+    imgSrc: "/thumbnails/SmartBeachThumbnail.webp",
+    imgAlt: "Budget Capital Thumbnail",
+    description:
+      "Budget Capital is a full-stack budgeting app developed as a practice project using the MERN stack. It allows users to manage their finances effectively by tracking income and expenses, categorizing transactions, and visualizing data through pie charts. Users can also set monthly budgets and view their financial history or future projections. Built collaboratively with a friend, the app focuses on user-friendly functionality and practical financial insights.",
+    githubUrl: "https://github.com/Jenna-Deamer/Budget-Capital",
+    demoUrl: "https://budget-capital-frontend.onrender.com/",
+    AriaLabel: "Visit live Demo of Budget Capital",
+  },
+];
+
+export default projectCards;
