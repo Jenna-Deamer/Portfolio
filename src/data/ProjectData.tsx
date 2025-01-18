9c48bc0fc8af72ce61ab7e6c93af01a06de12539
Jenna Deamer <jennadeamer@gmail.com>
2025-01-17 19:11:15 -0500

    Update Budget Capital project description for clarity

--- src/data/ProjectData.tsx
+++ src/data/ProjectData.tsx
@@ -14,7 +14,7 @@ const projectCards = [
     imgSrc: "/thumbnails/BudgetAppThumbnail.webp",
     imgAlt: "Budget Capital Thumbnail",
     description:
-      "Budget Capital is a full-stack budgeting app developed as a practice project using the MERN stack. It allows users to manage their finances effectively by tracking income and expenses, categorizing transactions, and visualizing data through pie charts. Users can also set monthly budgets and view their financial history or future projections. Built collaboratively with a friend, the app focuses on user-friendly functionality and practical financial insights.",
+      "Budget Capital is a full-stack budgeting app developed as a practice project using the MERN stack. It allows users to manage their finances effectively by tracking income and expenses, categorizing transactions, and visualizing data through pie charts. Users can also set monthly budgets and view their financial history. Built collaboratively with a friend, the app focuses on user-friendly functionality and practical financial insights.",
     githubUrl: "https://github.com/Jenna-Deamer/Budget-Capital",
     demoUrl: "https://budget-capital-frontend.onrender.com/",
     AriaLabel: "Visit live Demo of Budget Capital",
